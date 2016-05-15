eec1e2b06024a7b7c109432f068c78bfbed882f6
Alan <alan@govhack.org>
2016-05-15 11:49:42 +0000

    Replaced some carry-over mailgun passwords to Mandrill passwords (since we are a Mailchimp shop) Reincarnated the production "ghost.db" file Tweaked tag.hbs to bring it in line with index/post/page

--- config.js
+++ config.js
@@ -9,17 +9,19 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blog's published URL.
-        url: 'http://blog.govhack.org',
+        url: 'http://test1.ghost.alanyeung.net',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
         mail: {
             transport: 'SMTP',
             options: {
-                service: 'Mailgun',
+                host: 'smtp.mandrillapp.com',
+                port: 587,
+                service: 'Mandrill',
                 auth: {
-                    user: 'postmaster@mg.take-in.com.au', // mailgun username
-                    pass: '25d30ae03f476e7f41f2bf36992dd742'  // mailgun password
+                    user: process.env.MANDRILL_USERNAME, 
+                    pass: process.env.MANDRILL_PASSWORD
                 }
             }
         },
@@ -42,6 +44,36 @@ config = {
         },
         // #### Paths
         // Specify where your content directory lives
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        }
+    },
+
+    production: {
+        url: 'http://blog.govhack.org',
+        mail: {
+            transport: 'SMTP',
+            options: {
+                host: 'smtp.mandrillapp.com',
+                port: 587,
+                service: 'Mandrill',
+                auth: {
+                    user: process.env.MANDRILL_USERNAME, 
+                    pass: process.env.MANDRILL_PASSWORD
+                }
+            }
+        },
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            host: '0.0.0.0',
+            port: '2346'
+        },
         paths: {
             contentPath: path.join(__dirname, '/content/')
         }
